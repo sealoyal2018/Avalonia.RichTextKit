0cb8649ddd7773a4cbc092339af0be868579a68f
sealoyal <sealoyal@hotmail.com>
2024-11-05 18:00:50 +0800

    渲染

--- Avalonia.RichTextKit/Rendering/TextView.cs
+++ Avalonia.RichTextKit/Rendering/TextView.cs
@@ -1,10 +1,37 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Media;
+using Avalonia.RichTextKit.Models;
 
 namespace Avalonia.RichTextKit.Rendering;
 
-public class TextView : Control
+public class TextView: Control
 {
+    private readonly List<VisualTextBlock> textBlocks = [];
+
+    private static readonly StyledProperty<DomDocument> DomcumentProperty;
+    public DomDocument Domcument
+    {
+        get => GetValue(DomcumentProperty);
+        set => SetValue(DomcumentProperty, value);
+    }
+
+    static TextView()
+    {
+        DomcumentProperty = AvaloniaProperty.Register<TextView, DomDocument>(nameof(DomDocument), new DomDocument());
+        AffectsMeasure<TextView>(DomcumentProperty);
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        return base.MeasureOverride(availableSize);
+    }
+
+    protected override Size ArrangeOverride(Size finalSize)
+    {
+        return base.ArrangeOverride(finalSize);
+    }
+
+
     public override void Render(DrawingContext context)
     {
         context.DrawEllipse(Brushes.Peru, null, new Point(50, 50), 30, 50);
